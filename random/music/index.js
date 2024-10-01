b5777ec04d9df19f7e3c1b5e136c60635f7a92d9
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-10-01 19:42:41 -0300

    Refactor random music

--- random/music/index.js
+++ random/music/index.js
@@ -1,3 +1,6 @@
-let archor = document.querySelector("#goToMusic");
-archor.href = "https://www.youtube.com/results?search_query=" + (genres[Math.floor(Math.random() * (genres.length))]).replace(" ", "+") + "&sp=EgIQAw%253D%253D";
-archor.click();
+const anchor = document.querySelector("#goToMusic");
+const youtubeUrl = "https://www.youtube.com/results?search_query=";
+const musicSearch = (genres[Math.floor(Math.random() * (genres.length))]).replace(" ", "+");
+const onlyPlaylistsFilter = "&sp=EgIQAw%253D%253D";
+anchor.href = `${youtubeUrl}${musicSearch}${onlyPlaylistsFilter}`;
+anchor.click();
